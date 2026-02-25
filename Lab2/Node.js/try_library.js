75e842e9030e1e0d7302d7328051cedcde4bdade
Polina Kachan <kachan.polina8@gmail.com>
2026-02-25 16:40:42 +0200

    Index.html changed

---
+++ Lab2/Node.js/try_library.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+// Виконуємо GET-запит
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+        console.log('Статус:', response.status);
+        console.log('Дані:', response.data);
+    })
+    .catch(error => {
+        console.error('Помилка:', error.message);
+    });
