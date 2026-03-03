1f328606516067eb81d7467e54c53b240e2ee4ff
Polina Kachan <kachan.polina8@gmail.com>
2026-03-03 11:54:26 +0200

    Show response.headers

--- Lab2/Node.js/try_library.js
+++ Lab2/Node.js/try_library.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 // Виконуємо GET-запит
 axios.get('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => {
+        console.log('Заголовки:', response.headers);
         console.log('Статус:', response.status);
         console.log('Дані:', response.data);
     })
